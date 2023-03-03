869c2fcb8586101172d7d3a226125f881575ce11
misha <milisov4@gmal.com>
2023-03-03 21:59:12 +0200

    turbo broadcasting training

---
+++ app/controllers/accommodations_controller.rb
@@ -0,0 +1,41 @@
+# frozen_string_literal: true
+
+class AccommodationsController < ApplicationController
+  before_action :authenticate_user!, only: %i[new create]
+  before_action :find_user, only: :create
+
+  def index
+    @accommodations = Accommodation.all
+  end
+
+  def show
+    @accommodation = Accommodation.find_by(id: params[:id])
+  end
+
+  def new
+    @accommodation = Accommodation.new
+  end
+
+  def edit
+  end
+
+  def create
+    @accommodation = @user.accommodations.create!(accommodation_params)
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+  private
+
+  def accommodation_params
+    params.require(:accommodation).permit(:name, :user_id)
+  end
+
+  def find_user
+    @user = current_user
+  end
+end
--- app/models/accommodation.rb
+++ app/models/accommodation.rb
@@ -4,4 +4,12 @@ class Accommodation < ApplicationRecord
   belongs_to :user
 
   enum type: { hotel: 0, apartment: 1 }
+
+  after_create_commit lambda {
+    broadcast_append_to(
+      :list,
+      target: 'list-1',
+      partial: 'accommodations/accommodation'
+    )
+  }
 end
